aed1a9651db3d117c113d5e26562644aaae79941
Habib0 <31403162+Habib0@users.noreply.github.com>
2025-06-05 00:34:39 +0500

    Update thirdTest.cy.js

--- cypress/e2e/thirdTest.cy.js
+++ cypress/e2e/thirdTest.cy.js
@@ -3,7 +3,7 @@ const verifyUserEndpoint = Cypress.env('verifyUserEndpoint');
 const checkEmailEndpoint = Cypress.env('checkEmailEndpoint');
 
 describe('Bulk signup and report test', () => {
-  const numberOfUsers = 5;
+  const numberOfUsers = 1;
   const signupResults = [];
 
   it('Should signup 10 emails and verify them', () => {
